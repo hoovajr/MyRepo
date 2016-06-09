bbfd045d84b6fb450003319eab776c5eeec97850
Rob Hoover <hoovajr1@yahoo.com>
2016-06-08 23:12:19 -0400

    Initial pseudocode, potentnial constants defined

---
+++ OneNoteBackup/OneNoteBackup/OneNoteBackup.py
@@ -0,0 +1,12 @@
+SOURCE_FOLDER = 0
+DESTINATION_FOLDER = 0
+
+
+#instantiate new FileCopier
+
+# Set subfolder prefix
+# Set Source Folder, destination folder
+# Create Backup subfolder (prefix + current datetimestamp)
+# Copy Folders from source folder to newly created backup folder
+# If number of backups > 5, then delete the oldest backup
+
