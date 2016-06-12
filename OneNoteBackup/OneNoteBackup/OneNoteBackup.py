f261ba09385dfe1e4a06310f985fbd3dad3389e0
Rob Hoover <hoovajr1@yahoo.com>
2016-06-11 23:55:12 -0400

    Started writing instantiation code.

--- OneNoteBackup/OneNoteBackup/OneNoteBackup.py
+++ OneNoteBackup/OneNoteBackup/OneNoteBackup.py
@@ -1,10 +1,15 @@
 SOURCE_FOLDER = 0
 DESTINATION_FOLDER = 0
 
+# specifically will use shutil.copytree() in teh next 
 
 #instantiate new FileCopier
+oneNoteBackup = FileCopier()
 
-# Set subfolder prefix
+# Set prefix
+oneNoteBackup.set_prefix("test_dest")
+oneNoteBackup.
+
 # Set Source Folder, destination folder
 # Create Backup subfolder (prefix + current datetimestamp)
 # Copy Folders from source folder to newly created backup folder
