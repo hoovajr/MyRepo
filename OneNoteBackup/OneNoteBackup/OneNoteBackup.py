0e7e08cb2ec6b74d792ebf87579a6d90ab6c405a
Rob Hoover <hoovajr1@yahoo.com>
2016-06-16 23:19:28 -0400

    More changes and blanked out directories.

--- OneNoteBackup/OneNoteBackup/OneNoteBackup.py
+++ OneNoteBackup/OneNoteBackup/OneNoteBackup.py
@@ -4,13 +4,15 @@ import FolderCopier
 oneNoteBackup = FolderCopier.FolderCopier()
 
 # Set prefix
-oneNoteBackup.set_source_folder("C:\OneNoteNotebooks")
+oneNoteBackup.set_source_folder()
 oneNoteBackup.set_destination_folder(prefix="OneNote", 
-                                     dest_location="C:\OneNoteBackups")
+                                     dest_location="")
 oneNoteBackup.copy_folder()
 
 # Still to do:
-## How to deal with MyDocuments, and get that to work (e.g. double backslash etc.)
+## (done) (use r for raw strings) How to deal with MyDocuments, and get that to work (e.g. double backslash etc.)
+## If source_folder = dest_location, exit with custom exception.
+## Have source / destination folders passed in as arguments.
 ## If number of backups > 5, then delete the oldest backups starting with Prefix such that only 5 are remaining.
 ## Create batch file.
 ## Bonus - Create executable, annotate, and ensure PEP8 compliant.
