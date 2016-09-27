dbb90d72b529e79f0274b626fc546df5e5e528e6
Rob Hoover <hoovajr1@yahoo.com>
2016-09-26 21:46:49 -0400

    sycning all changes

--- FlaskTest2/FlaskTest2/FlaskTest2.py
+++ FlaskTest2/FlaskTest2/FlaskTest2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import vsearch
 
 app = Flask(__name__)
@@ -7,11 +7,18 @@ app = Flask(__name__)
 def hello() -> str: 
     return 'Hello world from Flask!' 
 
-@app.route('/search4')
-def do_search() -> str:
-    return str(vsearch.search4letters("life, the universe, and everything!", "eiru,!"))
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+
+    title = 'Here are your results.'
+
+    results = str(vsearch.search4letters(phrase, letters))
+    
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
-app.run()
+app.run(debug=True)
--- FlaskTest2/FlaskTest2/vsearch.py
+++ FlaskTest2/FlaskTest2/vsearch.py
@@ -4,6 +4,7 @@ def search4vowels(phrase:str) -> set:
     return vowels.intersection(set(phrase))
 
 def search4letters(phrase:str, letters:str='aeiou') -> set:
+    dfadsdsfdsfasddsaf
     """Return supplied letters in a suplied phrase"""
     return set(letters).intersection(set(phrase))
     
